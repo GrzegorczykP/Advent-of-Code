a0d1b0b941b332db005822ff0bca5537fa9ff3b5
GrzegorczykP <pawlo6697@gmail.com>
2020-12-22 15:27:12 +0100

    day 22 part 2 completed

--- 2020/22/solution.py
+++ 2020/22/solution.py
@@ -1,16 +1,34 @@
-def combat_sim(a, b):
-    if len(a) and len(b):
+def combat_sim(a, b, recursive=False, depth=0):
+    configs = set()
+
+    while len(a) and len(b):
         a0, b0 = a.pop(0), b.pop(0)
-        if a0 > b0:
+        curr = (tuple(a), tuple(b))
+        if curr in configs:
+            if depth == 0:
+                if len(b):
+                    a = b
+                print(sum([x * (len(a) - i) for i, x in enumerate(a)]))
+                return
+            else:
+                return True
+        configs.add(curr)
+        if recursive and a0 <= len(a) and b0 <= len(b):
+            if combat_sim(a[:a0], b[:b0], True, depth + 1):
+                a.extend([a0, b0])
+            else:
+                b.extend([b0, a0])
+        elif a0 > b0:
             a.extend([a0, b0])
         else:
             b.extend([b0, a0])
-        combat_sim(a, b)
     else:
-        if len(b):
-            a = b
-        len_ = len(a)
-        print(sum([x * (len_ - i) for i, x in enumerate(a)]))
+        if recursive and depth > 0:
+            return len(a) > 0  # True if Player I won
+        else:
+            if len(b):
+                a = b
+            print(sum([x * (len(a) - i) for i, x in enumerate(a)]))
 
 
 file = list(map(lambda x: x.strip(), open('input.txt')))
@@ -19,4 +37,7 @@ a = list(map(int, file[1:sep - 1]))
 b = list(map(int, file[sep + 1:]))
 
 # Part I
-combat_sim(a, b)
+# combat_sim(a, b)
+
+# Part II
+combat_sim(a, b, True)
