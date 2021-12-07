55209e5b11fc4a4d16649aeedbaa9cff822d22d2
GrzegorczykP <pawlo6697@gmail.com>
2021-12-07 21:29:44 +0100

    PHP day 1

---
+++ 2021/PHP/assignments/Day1.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Assignments;
+
+use BaseAssignment;
+
+class Day1 extends BaseAssignment
+{
+    private array $parsedData;
+
+    public function __construct(bool $isTest = false, int $day = 1)
+    {
+        parent::__construct($isTest, $day);
+        $this->parsedData = array_map('intval', explode(PHP_EOL, $this->inputData));
+    }
+
+    public function run(): array
+    {
+        return [
+            $this->run1(),
+            $this->run2()
+        ];
+    }
+
+    public function run1(): int
+    {
+        $dataCount = count($this->parsedData);
+        $inc = 0;
+
+        for ($i = 1; $i < $dataCount; $i++) {
+            if ($this->parsedData[$i - 1] < $this->parsedData[$i]) {
+                $inc++;
+            }
+        }
+        return $inc;
+    }
+
+    public function run2(): int
+    {
+        $dataCount = count($this->parsedData);
+        $inc = 0;
+
+        for ($i = 3; $i < $dataCount; $i++) {
+            $sumA = $this->parsedData[$i-1] + $this->parsedData[$i-2] + $this->parsedData[$i-3];
+            $sumB = $this->parsedData[$i] + $this->parsedData[$i-1] + $this->parsedData[$i-2];
+            if ($sumA < $sumB) {
+                $inc++;
+            }
+        }
+        return $inc;
+    }
+}
---
+++ 2021/PHP/index.php
@@ -0,0 +1,8 @@
+<?php
+require_once __DIR__ . '/base/init.php';
+
+$assigment = new Assignments\Day1();
+$result = $assigment->run();
+
+echo 'Task 1: ' . $result[0] . '<br>';
+echo 'Task 2: ' . $result[1];
